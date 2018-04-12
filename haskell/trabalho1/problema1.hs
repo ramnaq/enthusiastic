79c81243e7539c09d3a65bcf3fe01cc1045a8c5c
Ramna Sidharta <ramna.thors@gmail.com>
2018-04-11 21:23:02 -0300

    problema1 arrumado, add .pdf das questoes

--- haskell/trabalho1/problema1.hs
+++ haskell/trabalho1/problema1.hs
@@ -2,16 +2,17 @@ mesmosElementos :: [Int] -> [Int] -> Bool
 mesmosElementos [] [] = True
 mesmosElementos _ [] = False
 mesmosElementos [] _ = False
-mesmosElementos (head1:tail1) l2
-	| deletaElemento head1 l2 == l2 = False
-	| otherwise = mesmosElementos tail1 (deletaElemento head1 l2)
+mesmosElementos (h1:t1) (h2:t2)
+    | deletaElemento h1 (h2:t2) == (h2:t2) = False
+    | otherwise = mesmosElementos (deletaElemento h1 (h1:t1)) (deletaElemento h1 (h2:t2))
 
 deletaElemento :: Int -> [Int] -> [Int]
 deletaElemento _ [] = []
 deletaElemento n (head:tail)
-	| n == head = deletaElemento n tail  -- elemento pode se repetir
-	| otherwise = [head] ++ deletaElemento n tail
+    | n == head = deletaElemento n tail  -- elemento pode se repetir
+    | otherwise = [head] ++ deletaElemento n tail
 
 main = do
 	print $ mesmosElementos [1..5] [5, 1, 4, 3, 2, 1]        -- True
+	print $ mesmosElementos [5, 1, 4, 3, 2, 1] [1..5]        -- True
 	print $ mesmosElementos [1, 2, 3, 4, 5] [5, 1, 4, 2, 1]  -- False
