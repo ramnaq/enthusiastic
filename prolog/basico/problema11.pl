aed3a2adc1d15bdca81bce204fed5a493f4e2d2f
Ramna Sidharta <ramna.thors@gmail.com>
2018-06-09 18:51:34 -0300

    prolog: question 11 nº fibonacci

---
+++ prolog/basico/problema11.pl
@@ -0,0 +1,25 @@
+fib(N,K) :-
+  	N < 0, K is -1, !;
+	N =:= 0, K is 0, !;
+	N =:= 1, K is 1, !;
+	fib(N-2, A), fib(N-1, B), K is A + B.
+
+/* output
+?- fib(0,X).
+X = 0.
+
+?- fib(1,X).
+X = 1.
+
+?- fib(2,X).
+X = 1.
+
+?- fib(3,X).
+X = 2.
+
+?- fib(7,X).
+X = 13.
+
+?- fib(23,X).
+X = 28657.
+*/
