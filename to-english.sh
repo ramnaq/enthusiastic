ddd47e8a6583e762594c5854cdbee229195377eb
Ramna Sidharta <ramna.thors@gmail.com>
2018-06-24 12:36:49 -0300

    correcting script

--- to-english.sh
+++ to-english.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-for d in $1
-do
-cd "$d"
-for f in *; do
+cd $1
+for d in *; do
+  cd "$d"
+  for f in *; do
     file=$(basename -- "$f")
     extension="${file##*.}"
     f_name="${file%.*}"
-    en_name="$(echo "$f_name" | cut -d a -f 1)"
-    num="$(echo "$f_name" | cut -d a -f 2 | head -c 2 | tail -c 2)"
-    full_en_name="${en_name}${num}.${extension}"
-    fst_letter=$(echo "${f_name}" | head -c 1)
-    if [ $fst_letter == p ]
-    then
-        mv "$f" "$full_en_name"
-    fi
-done
+	en_name="$(echo "$f_name" | cut -d a -f 1)"  # problema -> problem
+	if [ $(echo "${f_name}" | head -c 1) == p ]
+	then
+	  num="$(echo "$f_name" | cut -d a -f 2 | head -c 2 | tail -c 2)"
+	  mv "$f" "${en_name}${num}.${extension}"  # problema<i>.<ext> -> problem<i>.ext
+	elif [ $(echo "${f_name}") == questoes ]
+	then
+	  mv "$f" questions.pdf	 # questoes.pdf -> questions.pdf
+	fi
+  done
+  cd ..
 done
