0b76eebbc916f9b83932891059dff38048725868
Ramna Sidharta <ramna.thors@gmail.com>
2018-06-24 13:02:10 -0300

    still renaming

--- to-english.sh
+++ to-english.sh
@@ -10,7 +10,7 @@ for d in *; do
 	if [ $(echo "${f_name}" | head -c 1) == p ]
 	then
 	  num="$(echo "$f_name" | cut -d a -f 2 | head -c 2 | tail -c 2)"
-	  mv "$f" "${en_name}${num}.${extension}"  # problema<i>.<ext> -> problem<i>.ext
+	  mv "$f" "${en_name}${num}.${extension}"  # problema<i>.<ext> -> problem<i>.<ext>
 	elif [ $(echo "${f_name}") == questoes ]
 	then
 	  mv "$f" questions.pdf	 # questoes.pdf -> questions.pdf
